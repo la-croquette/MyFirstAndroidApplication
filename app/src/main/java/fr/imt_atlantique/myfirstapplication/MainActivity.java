b1b03c7a27709025d5f53cd0401530d827fba7f4
la-croquette <zhan.ning@imt-atlantique.net>
2025-02-11 14:29:21 +0100

    add a menu to reset my application

--- app/src/main/java/fr/imt_atlantique/myfirstapplication/MainActivity.java
+++ app/src/main/java/fr/imt_atlantique/myfirstapplication/MainActivity.java
@@ -2,13 +2,21 @@ package fr.imt_atlantique.myfirstapplication;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,9 +26,9 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        // 确保 ID 使用的是 activity_main.xml 里正确的 ID
         View mainLayout = findViewById(R.id.main_layout);
-
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         if (mainLayout != null) {
             ViewCompat.setOnApplyWindowInsetsListener(mainLayout, (v, insets) -> {
                 Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -32,4 +40,56 @@ public class MainActivity extends AppCompatActivity {
             Log.e("Lifecycle", "Error: main_layout not found in activity_main.xml");
         }
     }
+
+    public void validateAction(View v) {
+
+        EditText lastName = findViewById(R.id.edit_last_name);
+        EditText firstName = findViewById(R.id.edit_first_name);
+        EditText birthDate = findViewById(R.id.edit_birth_date);
+        EditText birthCity = findViewById(R.id.edit_birth_city);
+
+
+        String textToShow = "Last Name: " + lastName.getText().toString() + "\n"
+                + "First Name: " + firstName.getText().toString() + "\n"
+                + "Birth Date: " + birthDate.getText().toString() + "\n"
+                + "Birth City: " + birthCity.getText().toString() ;
+
+        Snackbar snackbar = Snackbar.make(findViewById(R.id.main_layout), textToShow, Snackbar.LENGTH_LONG);
+
+        snackbar.setAction("DISMISS", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                snackbar.dismiss();
+            }
+        });
+        View snackbarView = snackbar.getView();
+        TextView snackbarText = snackbarView.findViewById(com.google.android.material.R.id.snackbar_text);
+
+        snackbarText.setMaxLines(5);
+        snackbarText.setSingleLine(false);
+        snackbar.show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public void resetAction(MenuItem item) {
+
+        EditText lastName = findViewById(R.id.edit_last_name);
+        EditText firstName = findViewById(R.id.edit_first_name);
+        EditText birthDate = findViewById(R.id.edit_birth_date);
+        EditText birthCity = findViewById(R.id.edit_birth_city);
+
+        lastName.setText("");
+        firstName.setText("");
+        birthDate.setText("");
+        birthCity.setText("");
+
+        Snackbar.make(findViewById(R.id.main_layout), "Fields have been reset", Snackbar.LENGTH_SHORT).show();
+    }
+
+
 }
